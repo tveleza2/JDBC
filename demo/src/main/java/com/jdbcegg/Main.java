3f9b8b8c38a4e0044afe8f7b436c8a37403a96c8
Tomas <pabloacosta@Tomas-Macbook.local>
2025-01-29 18:57:05 -0500

    DB connection created and tested

--- demo/src/main/java/com/jdbcegg/Main.java
+++ demo/src/main/java/com/jdbcegg/Main.java
@@ -1,7 +1,23 @@
 package com.jdbcegg;
+import java.sql.*;
+
+import com.jdbcegg.services.DbConnector;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection con = DbConnector.getConnection();
+        String query = "SELECT id_cliente, nombre_cliente FROM cliente";
+        try {
+            java.sql.PreparedStatement st = con.prepareStatement(query);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString("id_cliente") + " " + rs.getString("nombre_cliente"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            // TODO: handle exception
+        }
+        
+        
     }
 }
---
+++ demo/src/main/java/com/jdbcegg/services/DbConnector.java
@@ -0,0 +1,29 @@
+package com.jdbcegg.services;
+import java.sql.*;
+
+public class DbConnector {
+    private static String url="jdbc:mysql://localhost:3306/vivero";
+    private static String userName = "root";
+    private static String password = "Dbroot2024.";
+
+    public static Connection getConnection(){
+        try {
+            Connection con =  DriverManager.getConnection(url,userName,password);
+            System.out.println("Connection established successfully");
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+    public static void shutConnection(Connection con) {
+        if (con != null) {
+            try {
+                con.close();
+                System.out.println("Connection closed successfully");
+            } catch (SQLException e) {
+                System.out.println("Error al cerrar la conexión:" + e.getMessage());
+            }
+        }
+    }
+}
